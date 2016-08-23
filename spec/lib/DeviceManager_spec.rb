a12c24cedb277fb7f3ffdc3e0ad55b7636205d35
Rinnlinn <yhsuzu@gmail.com>
2016-08-23 23:20:33 +0900

    fix: DeviceManagerのテストコードの誤りを修正

--- spec/lib/DeviceManager_spec.rb
+++ spec/lib/DeviceManager_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe DeviceManager do
     dm = DeviceManager.new
     device = AdbDevice.new("192.168.1.5", 80)
     dm.add_device(device)
-    devices = dm.get_devices
+    devices = dm.getDevices
     result = false
     devices.each do |actual_device|
       if (actual_device.getIpAddress == "192.168.1.5" && actual_device.getPort == 80)
