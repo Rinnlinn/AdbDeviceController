2eb1de345d818e2bfc687b9f3c987835e3931fde
Rinnlinn <yhsuzu@gmail.com>
2016-08-23 23:08:35 +0900

    fix:テストコードのコミット漏れ

---
+++ spec/lib/DeviceManager_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../spec_helper'
+require_relative '../../src/DeviceManager.rb'
+require_relative '../../src/AdbDevice.rb'
+
+RSpec.describe DeviceManager do
+  it 'TestAddDevice' do
+    dm = DeviceManager.new
+    device = AdbDevice.new("192.168.1.5", 80)
+    dm.add_device(device)
+    devices = dm.get_devices
+    result = false
+    devices.each do |actual_device|
+      if (actual_device.getIpAddress == "192.168.1.5" && actual_device.getPort == 80)
+        result = true
+      end
+    end
+    expect(result).to eq true
+  end
+end
